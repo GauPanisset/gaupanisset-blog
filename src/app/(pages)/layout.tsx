80b23aee2d8488e1068ec3bf89caefc0e5995889
GauPanisset <gauthier.panisset@goshaba.com>
2024-01-01 13:21:18 +0100

    feat(opengraph-image): add og-image and twitter-image + add some space on top of page h1

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const PagesLayout: React.FunctionComponent<Props> = ({ children }) => {
   return (
-    <div className="mx-auto w-full max-w-[820px] p-6 sm:p-16 [&_h1]:duration-500 [&_h1]:animate-in [&_h1]:fill-mode-backwards motion-safe:[&_h1]:fade-in">
+    <div className="mx-auto w-full max-w-[820px] p-6 pt-16 sm:p-16 sm:pt-32 [&_h1]:duration-500 [&_h1]:animate-in [&_h1]:fill-mode-backwards motion-safe:[&_h1]:fade-in">
       {children}
     </div>
   );
