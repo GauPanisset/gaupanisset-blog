765b75e174f5fda992ce694b2f28bdd1e788d1ca
GauPanisset <gauthier.panisset@goshaba.com>
2024-03-02 12:17:36 +0100

    feat(articles): add 2 more projects (still need to write some words about them)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,13 +8,11 @@ import { NavigationBar } from '@/business/navigation/navigation-bar';
 import { ThemeProvider } from '@/business/theme/theme-provider';
 import { cn } from '@/technical/ui/helpers';
 
-const description =
-  "Gauthier's personal developer blog. Find his last projects here.";
-const title = 'Gauthier Panisset';
-
 const metadata: Metadata = {
-  description,
-  title,
+  description:
+    "Gauthier's personal developer blog. Find his last projects here.",
+  metadataBase: new URL('https://gauthier.panisset.fr'),
+  title: 'Gauthier Panisset',
 };
 
 const viewport: Viewport = {
@@ -23,7 +21,6 @@ const viewport: Viewport = {
 };
 
 const inter = Inter({
-  weight: ['400', '500', '700', '900'],
   subsets: ['latin'],
   variable: '--font-inter',
 });
@@ -34,14 +31,8 @@ type Props = {
 
 const RootLayout: React.FunctionComponent<Props> = ({ children }) => {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body
-        className={cn(
-          inter.className,
-          inter.variable,
-          'prose flex h-screen w-full max-w-none flex-col dark:prose-invert lg:prose-xl'
-        )}
-      >
+    <html lang="en" suppressHydrationWarning={true} className={inter.variable}>
+      <body className="prose flex h-screen w-full max-w-none flex-col dark:prose-invert lg:prose-xl">
         <ThemeProvider>
           <NavigationBar />
           <main className="m-auto flex min-h-0 w-full flex-1 flex-col overflow-auto">
